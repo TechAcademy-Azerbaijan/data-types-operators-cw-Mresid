01cd97c4f26c814863140ad997f808e986bed7d7
MacBook <macbook@Air-MacBook.bbrouter>
2022-08-13 05:35:07 -0700

    exercise

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,12 @@
+const { log } = require('console');
 const prompt = require('prompt');
 prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+  let input = result.input
+  let a = input.split(",");
+  let b = parseInt(a[0]);
+  let c = parseInt(b%10);
+  console.log(c);
 });
