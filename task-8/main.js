eb2d0f3902443654dae9efa142e77b667d8398a5
MacBook <macbook@Air-MacBook.bbrouter>
2022-08-13 06:50:52 -0700

    homework

--- task-8/main.js
+++ task-8/main.js
@@ -3,7 +3,9 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+  let a = parseInt(result.input);
+  let b = parseInt(a%10);//3
+  let c = parseInt(a/10);//12
+  let d = b*100+c;
+  console.log(d);
 });
