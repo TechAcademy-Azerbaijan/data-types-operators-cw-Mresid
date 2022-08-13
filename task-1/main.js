543f972baf018d3334622d03e7f240d2aa384be3
MacBook <macbook@Air-MacBook.bbrouter>
2022-08-13 07:58:26 -0700

    exercise

--- task-1/main.js
+++ task-1/main.js
@@ -1,9 +1,14 @@
 const prompt = require('prompt');
+const { parentPort } = require('worker_threads');
 prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+  let input = (result.input);//2,3,100
+  let a = input.split(",");
+  let b = parseInt(a[0]);//2
+  let c = parseInt(a[1]);//3
+  let d = parseInt(a[2]);//100
+  let x = b**c%d;
+  console.log(x);
 });
