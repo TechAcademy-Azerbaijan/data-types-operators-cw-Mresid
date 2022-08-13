06778ff71ab34af86aa9853966e50af06709f1d5
MacBook <macbook@Air-MacBook.bbrouter>
2022-08-13 04:56:44 -0700

    homework

--- task-4/main.js
+++ task-4/main.js
@@ -1,9 +1,14 @@
+const { lstat } = require('fs');
 const prompt = require('prompt');
 prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+  let input = (result.input);
+  let a = input.split(",");
+  let b = parseInt(a[0]);
+  let c = parseInt(a[1]);
+  let d = parseInt(b/c);
+  console.log(d);
 });
+
